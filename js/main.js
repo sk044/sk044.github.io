4bb5a570e488b00e6764551f28af32cbc5387656
Sk044 <shubham_1901mm32@iitp.ac.in>
2021-05-24 04:59:54 +0530

    update

--- js/main.js
+++ js/main.js
@@ -440,3 +440,87 @@ $(function() {
 
 });
 
+(function($){
+    $.fn.serializeObject = function(){
+
+        var self = this,
+            json = {},
+            push_counters = {},
+            patterns = {
+                "validate": /^[a-zA-Z][a-zA-Z0-9_]*(?:\[(?:\d*|[a-zA-Z0-9_]+)\])*$/,
+                "key":      /[a-zA-Z0-9_]+|(?=\[\])/g,
+                "push":     /^$/,
+                "fixed":    /^\d+$/,
+                "named":    /^[a-zA-Z0-9_]+$/
+            };
+
+
+        this.build = function(base, key, value){
+            base[key] = value;
+            return base;
+        };
+
+        this.push_counter = function(key){
+            if(push_counters[key] === undefined){
+                push_counters[key] = 0;
+            }
+            return push_counters[key]++;
+        };
+
+        $.each($(this).serializeArray(), function(){
+
+            // Skip invalid keys
+            if(!patterns.validate.test(this.name)){
+                return;
+            }
+
+            var k,
+                keys = this.name.match(patterns.key),
+                merge = this.value,
+                reverse_key = this.name;
+
+            while((k = keys.pop()) !== undefined){
+
+                // Adjust reverse_key
+                reverse_key = reverse_key.replace(new RegExp("\\[" + k + "\\]$"), '');
+
+                // Push
+                if(k.match(patterns.push)){
+                    merge = self.build([], self.push_counter(reverse_key), merge);
+                }
+
+                // Fixed
+                else if(k.match(patterns.fixed)){
+                    merge = self.build([], k, merge);
+                }
+
+                // Named
+                else if(k.match(patterns.named)){
+                    merge = self.build({}, k, merge);
+                }
+            }
+
+            json = $.extend(true, json, merge);
+        });
+
+        return json;
+    };
+})(jQuery);
+
+var $form = $('form#contactme'),
+    url = 'https://script.google.com/macros/s/AKfycbyfkPwPFY5dP6nROzn6WPWA2in0gl8JJOZE5vdfShPwmUNShmHOT-I7DoJWnq-qTkjf/exec'
+
+	
+
+	$('#submit-form').on('click', function(e) {
+	e.preventDefault();
+	var jqxhr = $.ajax({
+		url: url,
+		method: "GET",
+		dataType: "json",
+		data: $('#contactme').serializeObject()
+	}).success(
+		// alert("Your Message has been sent !!")
+		console.log($('#contactme').serializeObject()),alert("Response Submitted")
+	);
+	})
